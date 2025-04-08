833a7bd61b64226139c6f099afbada65e0bc9e1a
Martina <martugurevich@hotmail.com>
2025-04-08 13:44:24 +0000

    termine cpp de depa

--- ejercicio1/Departamento.cpp
+++ ejercicio1/Departamento.cpp
@@ -2,7 +2,10 @@
 
 //constructor de departamento 
 
-Departamento_:: Departamento_(const string& nombredepa,const string& ubi ) : nombre(nombredepa), ubicacion(ubi){}
+int Departamento_ :: cantEmpleadosDepts=0; //porque es static
+Departamento_:: Departamento_(const string& nombredepa,const string& ubi ) : nombre(nombredepa), ubicacion(ubi){
+    cantEmpleadosDepts++; //cada vez q se crea un depa aumento contador
+}
 
 //GETTERS
 string Departamento_::getNombre() {
@@ -13,8 +16,29 @@ string Departamento_::getUbicacion(){
     return ubicacion;
 }
 
-// int Departamento_:: contarEmpleados(){
-//     for (auto& emple: empleados){
-//         if()
-//     }
-// }
+
+int Departamento_:: contarEmpleados(){
+    return cantEmpleadosDepts; //o empleados.size() si quiro contar por instancia , no entre todos los depst
+}
+
+vector<shared_ptr<Empleado_>> Departamento_:: getEmployees(){
+    return empleados;
+}
+
+bool Departamento_:: contratarEmpleado(shared_ptr<Empleado_> emple){
+    empleados.push_back(emple);
+}
+
+bool Departamento_:: despedirEmpleado(shared_ptr<Empleado_> emple){
+    for (auto e = empleados.begin(); e != empleados.end(); ++e){
+        if ( *e == emple){
+            empleados.erase(e);
+            return true; //lo encontre 
+        }
+    }
+    return false;
+}
+
+
+
+
